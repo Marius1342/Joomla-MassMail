e6109981eb046217483d70a7ee45d979a1315886
Mariusk <90910912+Marius1342@users.noreply.github.com>
2022-04-15 13:12:52 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Joomla-MassMail
+
+*You can select users and write to them one email*
+
+* Why i should use this?
+    * ✅ Free to use
+    * ✅ You can select users from one group
+
+__Tested with Joomla 4.1 and php8.0.17__
+
+## Install
+- Create a custom HTML, paste the code from index.php in the php section
+- Save changes
+- You can only access the site if you are the superuser
+
+Fell free to open an <a href="https://github.com/Marius1342/joomla-welcome/issues">issue</a>, if you have any questions or ideas
---
+++ ts/main.ts
@@ -0,0 +1,62 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+if (urlParams.get('ok') != undefined) {
+    window.alert(urlParams.get('ok'));
+}
+var selected = <HTMLFormElement> document.getElementById('select');
+(<HTMLFormElement>document.getElementById('loc')).value = window.location.href;
+var form = <HTMLFormElement> document.getElementById('emails');
+var List = <HTMLFormElement> document.getElementById('list');
+var submit = <HTMLFormElement> document.getElementById('submite');
+var children = [''];
+var emails = [''];
+
+function CreatChild(username) {
+    var child = document.createElement('p');
+    child.addEventListener('click', function() {
+        RemoveElement(this.innerText);
+    });
+    child.innerText = username;
+    child.className = 'item';
+    return child;
+
+}
+
+function RemoveElement(username) {
+    if (!children.includes(username)) {
+        console.log('Error cant find ' + username);
+        return;
+    }
+    var index = children.indexOf(username);
+    console.log(children.splice(index, 1));
+    console.log(emails.splice(index, 1));
+    for (var i = 0; i < List.childNodes.length; i++) {
+        if ( (<HTMLInputElement>List.childNodes[i]).innerText == username) {
+            List.removeChild(List.childNodes[i]);
+        }
+
+    }
+    if (children.length > 1) {
+        submit.disabled = false;
+    } else {
+        submit.disabled = true;
+    }
+    form.value = JSON.stringify(emails);
+}
+
+function AddList(email, username) {
+    selected.selectedIndex = 0;
+    //Checks if the child already exists
+    if (children.includes(username)) {
+        window.alert('This user is already selected.');
+        return;
+    }
+    //Create and append child
+    var child = CreatChild(username);
+    List.appendChild(child);
+    children.push(username);
+    emails.push(email);
+    submit.disabled = false;
+    form.value = JSON.stringify(emails);
+
+}
